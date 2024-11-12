33260518fb9b096bd70e703e7e461414d918fdc4
andreytondo <andreytondo0408@gmail.com>
2024-11-12 00:45:53 -0300

    feat: disable csrf temporarilly for development

--- projeto-final/server/src/main/java/br/edu/utfpr/pb/pw44s/projetofinal/security/WebSecurity.java
+++ projeto-final/server/src/main/java/br/edu/utfpr/pb/pw44s/projetofinal/security/WebSecurity.java
@@ -3,12 +3,10 @@ package br.edu.utfpr.pb.pw44s.projetofinal.security;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +15,9 @@ public class WebSecurity {
     @Bean
     @SneakyThrows
     public SecurityFilterChain filterChain(HttpSecurity http) {
-        http.authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers(antMatcher(HttpMethod.GET,"/**")).permitAll()
-                .anyRequest().authenticated()
-        );
+        http
+            .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll()) // TODO configure security for other methods
+            .csrf(AbstractHttpConfigurer::disable); // TODO re-enable at end of development
         return http.build();
     }
 }
